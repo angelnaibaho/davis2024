c175d2d6a40dd3ce97424916c6ad92bece77ecb8
Angel Naibaho <101439456+angelnaibaho@users.noreply.github.com>
2024-05-07 16:30:20 +0800

    Update movieScrap.py

--- movieScrap.py
+++ movieScrap.py
@@ -1,10 +1,19 @@
 import streamlit as st
 import pandas as pd
  
-st.write("""
-# My first app
-Hello *world!*
-""")
- 
-df = pd.read_csv("BoxOfficeMojoScrap.csv")
-st.bar_chart(df)
+st.write("""# Book Scrap""")
+    
+# read dataframe dari file CSV
+df = pd.read_csv("BookScrape.csv")
+
+# Pilih kolom judul dan harga
+df_selected = df[['TITLE', 'PRICE']]
+
+# Buat scatter plot dengan Plotly Express
+fig = px.scatter(df_selected, x='PRICE', y='TITLE')
+
+# Konfigurasi tampilan scatter plot
+fig.update_traces(marker=dict(size=5, color='skyblue', opacity=1), selector=dict(mode='markers'))
+
+# Menampilkan scatter plot
+st.plotly_chart(fig)
