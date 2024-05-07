1ab5c990a555b644adcadd56eec3285716217920
Angel Naibaho <101439456+angelnaibaho@users.noreply.github.com>
2024-05-07 18:43:09 +0800

    Update tips.py

--- tips.py
+++ tips.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+# PLOT SCATTER
 st.write("""# Data Visual""")
 st.write("""Anna Vita Angelina Naibaho - 21082010135""")
 # Load data
@@ -20,6 +22,24 @@ ax.set_ylabel('Tip')
 
 # Add colorbar
 plt.colorbar(scatter, ax=ax)
+
+# Show plot
+st.pyplot(fig)
+
+# Nomor 2 
+# Load data
+data = pd.read_csv("tips.csv")
+
+# Create bar chart
+fig, ax = plt.subplots()
+ax.bar(data['day'], data['total_bill'])
+
+# Adding Title to the Plot
+ax.set_title("Bar Chart")
+
+# Setting the X and Y labels
+ax.set_xlabel('day')
+ax.set_ylabel('total_bill')
 
 # Show plot
 st.pyplot(fig)
