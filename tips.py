552c774ed720fc0142c557ec70ae1785a5f30dc7
Angel Naibaho <101439456+angelnaibaho@users.noreply.github.com>
2024-05-07 17:49:41 +0800

    Update tips.py

--- tips.py
+++ tips.py
@@ -1,24 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#PLOT BIASA
-# reading the database
-data = pd.read_csv("tips.csv")
-
-# Scatter plot with day against tip
-plt.scatter(data['day'], data['tip'])
-
-# Adding Title to the Plot
-plt.title("Scatter Plot")
-
-# Setting the X and Y labels
-plt.xlabel('Day')
-plt.ylabel('Tip')
-
-plt.show()
-
-
-#PLOT COLOR
+# PLOT COLOR
 data = pd.read_csv("tips.csv")
 
 # Scatter plot with day against tip
