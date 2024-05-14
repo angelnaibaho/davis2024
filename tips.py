f5bea255fb08034fdde65a676af26e4bc222a1a0
Angel Naibaho <101439456+angelnaibaho@users.noreply.github.com>
2024-05-14 14:22:09 +0700

    Update tips.py

--- tips.py
+++ tips.py
@@ -4,6 +4,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
+
+# Initialize connection.
+# conn = st.connection('mysql', type='sql', username=st.secrets["DB_USER"], password=st.secrets["DB_PASS"], host=st.secrets["HOST"], database=st.secrets["DB"])
+# conn = st.connection(**st.secrets.db_credentials)
+conn = st.connection("mydb", type="sql", autocommit=True)
+# st.write(st.secrets["connections.mydb"]["username"])
+# Perform query.
+df = conn.query('SELECT EnglishPromotionName, StartDate, EndDate, MaxQty from dimpromotion limit 10;', ttl=600)
+
+st.table(df)
+# Print results.
+
 # PLOT SCATTER
 st.write("""# Data Visual""")
 st.write("""Anna Vita Angelina Naibaho - 21082010135""")
